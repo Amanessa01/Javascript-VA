63b96fc1555fb0da8c505cb02b1c75a77b0045d4
Amanessa01 <112988303+Amanessa01@users.noreply.github.com>
2022-12-14 18:28:41 +0000

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+//*psedocode for rock paper scissors activity*//
+
+If rock competes with scissors, rock will win
+If scissors competes with paper, scissors will win
+If rock competes with paper, paper will win
+If rock compeptes with rock, players will draw
+If scissors competes with scissors, players will draw
+If paper competes with paper, players will draw
+
+//* The above translated into code*//
