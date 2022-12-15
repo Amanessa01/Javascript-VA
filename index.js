7308921ca93ca32f42862abcbf744400e041bad3
Amanessa01 <112988303+Amanessa01@users.noreply.github.com>
2022-12-15 20:36:04 +0000

    Update index.js

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ If paper competes with paper, players will draw
 
 
 I want to play Rock, Paper, Scissors against an automated opponent.
+var rps= ["rock","paper","scissors"
 I can enter R, P, or S to signify my choice of rock, paper, or scissors.
 I expect the computer to choose R, P, or S in return.
 I want the option to play again whether I win or lose.
@@ -22,3 +23,21 @@ I want to see my total wins, ties, and losses after 10 rounds.
 
 
 The computer's selection must be random to ensure a fair game
+----------------------------------------------------------------
+ Correct format for psudocode:
+          
+// Is there going to be a computer compnent?
+// What's the first step?
+// Renact RPS
+// How to submit my own choise for r.p.s
+   
+// Prompt the user to enter "r," "p," "s"
+// How is the oppnenet going to retrieve their choice?
+// Initialise 'Choices' array r.p.s representing rock,paper, scissors
+// Computer chooses a **random** value from the array of "r," "p," "s" using math.random
+// Do I need multiple rounds? Do I need to track wins, loses and ties? I do need to tracks wins, losses and ties
+// Variables then set up a bunch of if/else statements that checks for win/loss/tie conditions
+          . For either of the choicees, increment the result in the choice variable. For example if the user choice ==== "r" && computerchoice ==== "s" [win
+ alert("You Win"):
+ Do I need to prompt user play again? Yes that way there are multiple games
+ 
